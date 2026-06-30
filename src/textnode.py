5e6c8622947875849d0d710580cfa464d4dfeb87
AJebus <alexjesus1995@gmail.com>
2026-06-30 11:31:19 +0100

    CH2: L2: TextNode Tests

--- src/textnode.py
+++ src/textnode.py
@@ -1,21 +1,27 @@
 from enum import Enum
 
+
 class TextType(Enum):
-    text = "plain"
-    bold_text = "bold"
-    italic_text = "italic"
-    code_text = "code"
-    link = "link"
-    image = "image"
+    TEXT = "plain"
+    BOLD = "bold"
+    ITALIC = "italic"
+    CODE = "code"
+    LINK = "link"
+    IMAGE = "image"
+
 
 class TextNode:
-    def __init__(self, content: str, text_type: str, url: str | None):
+    def __init__(self, content: str, text_type: str, url: str | None = None):
         self.text = content
         self.text_type = TextType(text_type)
         self.url = url
 
     def __eq__(self, other: TextNode):
-        return self.text == other.text and self.text_type == other.text_type and self.url == other.url
+        return (
+            self.text == other.text
+            and self.text_type == other.text_type
+            and self.url == other.url
+        )
 
     def __repr__(self):
         return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
